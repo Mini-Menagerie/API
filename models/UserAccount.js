c336e167c6295ff8620e36bda8b01e7362068f4f
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2020-09-16 18:24:48 +0700

    login google

--- models/UserAccount.js
+++ models/UserAccount.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const userAccountSchema = new Schema({
+    providerId: {
+        type: String
+    },
+    providerName: {
+        type: String
+    },
     idUser: {
         type: Schema.Types.ObjectId,
         ref: 'users',
