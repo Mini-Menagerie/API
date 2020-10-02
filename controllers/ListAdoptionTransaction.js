db09cb323f416976b358dff877b3cf1f4f751911
AgungMubarok <agungmubarok555@gmail.com>
2020-10-02 22:54:46 +0700

    populate

--- controllers/ListAdoptionTransaction.js
+++ controllers/ListAdoptionTransaction.js
@@ -30,7 +30,7 @@ module.exports = {
             .populate({
                 path: "idPetUpForAdoption",
                 populate: {
-                    path: "idUser",
+                    path: "idPet",
                 },
             })
             .populate({ 
